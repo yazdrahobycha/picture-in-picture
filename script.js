ba3a9a064b5f072be3a3ad8253c89b952d421393
ArsenYavorskyi <ars.yar24@gmail.com>
2023-04-04 12:42:05 +0300

    added functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        };
+    } catch (error) {
+        alarm('something went wrong' + error);
+    }
+}
+
+// Event listener for button click to start picture in picture 
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+})
+
+// on load
+selectMediaStream();
